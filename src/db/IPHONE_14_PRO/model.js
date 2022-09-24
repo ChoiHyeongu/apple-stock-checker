d746cfe99942c21821846762a5d414ad3d7932e2
ChoiHyeongu <a11dsd123@gmail.com>
2022-09-24 15:45:39 +0900

    fix: SPEC 변수명 겹치는 부분 수정

--- src/db/IPHONE_14_PRO/model.js
+++ src/db/IPHONE_14_PRO/model.js
@@ -1,24 +1,24 @@
-import { IPHONE_14_PRO } from './specs.js';
+import { IPHONE_14_PRO as SPEC } from './specs.js';
 
 export const IPHONE_14_PRO = {
   'MQ0G3KH/A': {
-    size: IPHONE_14_PRO.SIZE._14_RPO,
-    color: IPHONE_14_PRO.COLOR.DEEP_PURPLE,
-    storage: IPHONE_14_PRO.STORAGE._128GB,
+    size: SPEC.SIZE._14_RPO,
+    color: SPEC.COLOR.DEEP_PURPLE,
+    storage: SPEC.STORAGE._128GB,
   },
   'MPXV3KH/A': {
-    size: IPHONE_14_PRO.SIZE._14_RPO,
-    color: IPHONE_14_PRO.COLOR.SPACE_BLACK,
-    storage: IPHONE_14_PRO.STORAGE._128GB,
+    size: SPEC.SIZE._14_RPO,
+    color: SPEC.COLOR.SPACE_BLACK,
+    storage: SPEC.STORAGE._128GB,
   },
   'MQ183KH/A': {
-    size: IPHONE_14_PRO.SIZE._14_RPO,
-    color: IPHONE_14_PRO.COLOR.GOLD,
-    storage: IPHONE_14_PRO.STORAGE._256GB,
+    size: SPEC.SIZE._14_RPO,
+    color: SPEC.COLOR.GOLD,
+    storage: SPEC.STORAGE._256GB,
   },
   'MQ1W3KH/A': {
-    size: IPHONE_14_PRO.SIZE._14_RPO,
-    color: IPHONE_14_PRO.COLOR.SILVER,
-    storage: IPHONE_14_PRO.STORAGE._512GB,
+    size: SPEC.SIZE._14_RPO,
+    color: SPEC.COLOR.SILVER,
+    storage: SPEC.STORAGE._512GB,
   },
 };
