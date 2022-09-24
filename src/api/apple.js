5fc5ec13b9970a931620849071c44c2363c02489
ChoiHyeongu <a11dsd123@gmail.com>
2022-09-24 15:50:14 +0900

    refact: 테스트 코드 제거

--- src/api/apple.js
+++ src/api/apple.js
@@ -8,17 +8,5 @@ import { sku } from '../db/sku.js';
  * @returns
  */
 export function getStock({ productCode, location }) {
-  const url = createURL();
-  return request.get(url);
   return request.get(`shop/fulfillment-messages?pl=true&mts.0=regular&parts.0=${productCode}&location=${location}`);
 }
-
-const createURL = () => {
-  let url = 'shop/fulfillment-messages?pl=true&mts.0=regular';
-
-  sku.slice(0, 10).forEach((v, i) => {
-    url = url + `&parts.${i}=${v}KH/A`;
-  });
-
-  return url;
-};
